7d286b7489f5f727be76aabdfd14740705b83df2
viniciusfal <vinicius_prsilva@hotmail.com>
2024-04-26 18:29:25 -0300

    config: Deploy v2

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,9 +6,9 @@ import { DefaultLayout } from './layout/DefaultLayout'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/history" element={<History />} />
+      <Route path="/<pomodoro>" element={<DefaultLayout />}>
+        <Route path="/<pomodoro>" element={<Home />} />
+        <Route path="/<pomodoro>/history" element={<History />} />
       </Route>
     </Routes>
   )
